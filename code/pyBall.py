2f012cee3ec966619da9e0053e8e059879442090
Damian <drsowinski@gmail.com>
2017-11-30 12:52:24 -0500

    Added pyBall and scripts
    pyBall: the functions I use to extract the phase space of the ball
    scripts: plotting scripts for the marginals

---
+++ code/pyBall.py
@@ -0,0 +1,104 @@
+#---------------------------------------------------LIBRARIES
+import json
+import numpy as np
+import os
+import tqdm
+import matplotlib
+
+matplotlib.rcParams["mathtext.fontset"] = "stix"
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+#---------------------------------------------------DATA HANDLING FUNCTIONS
+
+def ball_phase_space_generate(directory_name):
+    """Extracts all the points in phase space of the ball from 
+    the files in the directory ( a season).  
+    
+    INPUT
+    directory_name  name of the directory holding the json files
+    
+    OUTPUT
+    """
+    file_names = os.listdir(directory_name)
+    coordinates = []
+    for file_name in tqdm.tqdm(file_names):
+        game_data = ball_data_load(directory_name + file_name)
+        if len(game_data) > 0:
+            for point in game_data:
+                coordinates.append(point)
+    return np.array(coordinates)
+
+def ball_data_load(file_name):
+    """Loads the phase-space coordinates, (x,y,z,vx,vy,vz,...), for all 
+    the frames of a game.
+    
+    INPUT
+    file_name       The location of the file being read
+    
+    OUTPUT
+    coordinates     An array of arrays. The top layer is the contiguous trajectories.
+                    The next layer is the array of phase space coordinates of the ball 
+                    along that contiguous trajectory. 
+    """
+    coordinates = []
+    #this is the prjector that picks out the time stamp and spatial coordinates of
+    # the ball from the moment data
+    
+    try:
+        #Check to make sure that the file is a json file
+        if _check_json(file_name):
+            json_strs = _get_json_str(file_name)
+            #This loop goes over all the contiguous trajectories in the game
+            for json_str in json_strs:
+                try:
+                    moments = json.loads(json_str)['moments']
+                    if len(moments)>5:
+                        #this is where the data is projected into the coordinate list
+                        trajectory = np.array(list(map(_coordinate_projection,moments))).T
+                        #adds the velocity, acceleration, etc
+                        trajectory = _add_velocity(trajectory)
+                        for point in trajectory[1:].T:
+                            if _validate_point(point):
+                                coordinates.append(point)
+                except ValueError:
+                    pass
+    except IndexError:
+        pass
+    return np.array(coordinates)
+
+def _validate_point(point):
+    #Validates a point by checking to make sure it is in the boundary of the court,
+    #and that the speeds are not too large (which is a sign of an artifact coming from
+    #the finite difference method)
+    return ((point[0]<=94)&(point[0]>=0)&(point[1]<=50)&(point[1]>=0)&(point[2]>0)&
+            (np.abs(point[3])<40)&(np.abs(point[4])<40)&(np.abs(point[5])<40))
+
+def _coordinate_projection(moment):
+    #Projects the (t,x,y,z) coordinates of the ball from the moment structure
+    return np.array([moment[2],moment[5][0][2],moment[5][0][3],moment[5][0][4]])
+
+def _check_json(file_name):
+    #checks to see if the extension of a file is .json
+    return file_name[-4:]=='json'
+
+def _get_json_str(file_name):
+    json_file = open(file_name,'r')
+    return json_file.read().split('\n')
+
+def _add_velocity(trajectory):
+    dt = np.convolve(trajectory[0],[-1,0,1],'valid')
+    #valid indices are those in which the clock is running
+    valid_idx = dt!=0
+    dt = dt[valid_idx]
+    #smooth the coordinates
+    t = np.convolve(trajectory[0],[.25,.5,.25],'valid')[valid_idx]
+    x = np.convolve(trajectory[1],[.25,.5,.25],'valid')[valid_idx]
+    y = np.convolve(trajectory[2],[.25,.5,.25],'valid')[valid_idx]
+    z = np.convolve(trajectory[3],[.25,.5,.25],'valid')[valid_idx]
+    #central difference method for the velocity, quadratic error
+    dx = np.convolve(trajectory[1],[-1,0,1],'valid')[valid_idx]
+    dy = np.convolve(trajectory[2],[-1,0,1],'valid')[valid_idx]
+    dz = np.convolve(trajectory[3],[-1,0,1],'valid')[valid_idx]
+    vx = dx/dt
+    vy = dy/dt
+    vz = dz/dt
+    return np.array([t,x,y,z,vx,vy,vz])
---
+++ code/scripts.py
@@ -0,0 +1,74 @@
+import json
+import numpy as np
+import os
+import tqdm
+from scipy.ndimage.filters import gaussian_filter as smooth
+
+
+matplotlib.rcParams["mathtext.fontset"] = "stix"
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+
+#----------------------------------- 1d Marginals 
+hx = np.histogram(coordinates[0],bins = 940,normed=True)
+hy = np.histogram(coordinates[1],bins = 500,normed=True)
+hz = np.histogram(coordinates[2],bins = 1000,normed=True)
+
+fig = plt.figure(n,figsize = (12,10))
+ax1 = plt.axes([.08,.08,.4,.25])
+plt.xlabel('$z$(ft)')
+ax1.plot(hz[1][1:],hz[0])
+ax2 = plt.axes([.08,.08+.06+.25,.4,.25])
+plt.xlabel('$y$(ft)')
+ax2.plot(hy[1][1:],hy[0])
+ax3 = plt.axes([.08,.08+.06+.25+.06+.25,.4,.25])
+plt.xlabel('$x$(ft)')
+ax3.plot(hx[1][1:],hx[0])
+ax3.set_title('Position Marginals')
+ax4 = plt.axes([.08+.48,.08,.4,.25])
+plt.xlabel('$v_z$')
+ax4.plot(hvz[1][1:],hvz[0])
+ax5 = plt.axes([.08+.48,.08+.06+.25,.4,.25])
+plt.xlabel('$v_y$')
+ax5.plot(hvy[1][1:],hvy[0])
+ax6 = plt.axes([.08+.48,.08+.06+.25+.06+.25,.4,.25])
+plt.xlabel('$v_x$')
+ax6.plot(hvx[1][1:],hvx[0])
+ax6.set_title('Velocity Marginals')
+
+#----------------------------------- 2d Marginals
+h = map(list,np.ones([6,6]))
+for col in range(6):
+    for row in range(col+1):
+        if row == col:
+            h[row][col] = np.histogram(coordinates[row],bins = 1000,normed=True)
+        else:
+            h[row][col]=np.histogram2d(coordinates[row],coordinates[col],bins = (1000,1000),normed = True)
+
+fig = plt.figure(1,figsize = (12,12))
+labels = ['$x$','$y$','$z$','$v_x$','$v_y$','$v_z$']
+plt.clf()
+for col in range(6):
+    for row in range(col+1):
+        print(row,col)
+        ax = plt.axes([.05+col*.15,.05+row*.15,.15,.15])
+        if row == col:
+            x = h[row][col][1][1:]
+            y = h[row][col][0]
+            ax.plot(x,y,linewidth=.5)
+            plt.ylabel(labels[row],rotation = 0)
+        else:
+            z = h[row][col][0]
+            z[z==0] = min(z[z>0])
+            z = smooth(z,10)
+            ax.contour(np.log(z),100,linewidths = .2,linestyles='solid',colors = 'black',alpha = .5)
+            ax.imshow(z,alpha = .8)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        if row==0:
+            plt.xlabel(labels[col])
+
+#-------------------------------------- Kinetic Energy
+
+            
+        
